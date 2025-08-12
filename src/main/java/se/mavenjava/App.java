cbc6fe8a23ae2ca198212cfa9cb3436a311ce9a7
akshayabelde <akshayabelde@gmail.com>
2025-08-12 11:47:51 +0530

    first commit

---
+++ src/main/java/se/mavenjava/App.java
@@ -0,0 +1,13 @@
+package se.mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "SE LAB!" );
+    }
+}
